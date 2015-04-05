7801e8803c1b08bbe5f45468de906220f6873123
cristo-rabani <cristo.rabani@gmail.com>
2015-04-05 22:24:48 +0200

    up

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
 
-====1.0.5====
+====1.0.6====
 -------------
 - deprecated function 'getUniUserObject'
 
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: 'Many awesome utilities',
     name: 'vazco:universe-utilities',
-    version: '1.0.5',
+    version: '1.0.6',
     git: 'https://github.com/vazco/meteor-universe-utilities'
 });
 
