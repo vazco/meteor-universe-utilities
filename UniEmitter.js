1a602a54a52657c0bc212073b277deab1ad953bc
cristo-rabani <cristo.rabani@gmail.com>
2015-10-17 15:00:11 +0200

    up

--- UniEmitter.js
+++ UniEmitter.js
@@ -15,7 +15,7 @@ UniUtils.Emitter.prototype.emit = function emit(eventType) {
 };
 
 UniUtils.Emitter.prototype.on = function on(eventType, listener) {
-    if (!_.isArray(this._listeners[eventType])) {
+    if (!Array.isArray(this._listeners[eventType])) {
         this._listeners[eventType] = [];
     }
 
@@ -28,29 +28,24 @@ UniUtils.Emitter.prototype.on = function on(eventType, listener) {
 
 UniUtils.Emitter.prototype.once = function once(eventType, listener) {
     var self = this;
-
     function _once() {
         var args = Array.prototype.slice.call(arguments, 0);
         self.off(eventType, _once);
         listener.apply(self, args);
     }
-
     _once.listener = listener;
     return this.on(eventType, _once);
 };
 
 UniUtils.Emitter.prototype.off = function off(eventType, listener) {
-    if (!_.isArray(this._listeners[eventType])) {
+    if (!Array.isArray(this._listeners[eventType])) {
         return this;
     }
-
     if (typeof listener === 'undefined') {
         this._listeners[eventType] = [];
         return this;
     }
-
     var index = this._listeners[eventType].indexOf(listener);
-
     if (index === -1) {
         for (var i = 0; i < this._listeners[eventType].length; i += 1) {
             if (this._listeners[eventType][i].listener === listener) {
