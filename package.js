d22a14c444222bf28722714e30ba4e3842604ddc
Cristo <cristo.rabani@gmail.com>
2015-10-24 08:56:14 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: 'Many awesome utilities',
     name: 'universe:utilities',
-    version: '2.0.7',
+    version: '2.0.8',
     git: 'https://github.com/vazco/meteor-universe-utilities'
 });
 
